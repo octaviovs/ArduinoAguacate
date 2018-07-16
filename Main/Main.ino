054b2401c3d55f2cc6d21e4125885a492e46c73d
OctavioMac <octaviogomezromohotmail.com>
2018-07-16 18:03:58 -0500

    Clase motor, para ultilizar servos

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,21 +1,13 @@
 #include "motor.h"
 
-const int rele = 0;
+motor Banda;
 
-/***   Setup  ***/
+
 void setup() {
-    pinMode(rele,OUTPUT);
-     pinMode(1,OUTPUT);
+
 }
 
-/***   Loop  ***/
+
 void loop() {
-    digitalWrite(rele, 0);
-      digitalWrite(1, 0);
-    delay(500);
-     digitalWrite(rele, 1);
-     digitalWrite(1, 1);
-      delay(300);
-     digitalWrite(rele, 0);
-          digitalWrite(1, 0);
+
 }
--- Main/motor.h
+++ Main/motor.h
@@ -1,11 +1,33 @@
- #include <Servo.h>
+#include <Servo.h>
 
- class motor
+class motor
 {
-  public:
-    Morse(int pin);
-    void dot();
-    void dash();
   private:
-    int _pin;
+    //variables que pertenceran a los dos motores de la banda
+    Servo s1;
+    Servo s2;
+
+  public:
+    // constructor vacio
+    moto() {
+    }
+    //constrcutor para iniciarlizar las variables de tipo servo
+    moto(int x, int y) {
+      s1.attach(x);
+      s2.attach(y);
+    }
+
+    void alto() {
+       s1.write(90);
+       s2.write(90);
+    }
+    void adelante() {
+       s1.write(0);
+       s2.write(180);
+    }
+    void atras() {
+       s1.write(180);
+       s2.write(0);
+    }
+
 };
